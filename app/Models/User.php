3edb93dda45cfc7c30fd9ac920149f4c930fff8e
xiehouyou <1617835576@qq.com>
2018-09-11 16:56:35 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,9 @@
 <?php
-
-namespace App\models;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use App\Models\User;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -28,4 +27,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+     public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }

fb1f2f354ce2b7dcce51431734ae1987fa200b66
xiehouyou <1617835576@qq.com>
2018-09-14 05:20:06 +0000

    Finish login and logout

--- public/js/app.js
+++ public/js/app.js
@@ -1027,15 +1027,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * all outgoing HTTP requests automatically have it attached. This is just
  * a simple convenience so we don't have to attach every token manually.
  */
-
-var token = document.head.querySelector('meta[name="csrf-token"]');
+/*
+*默认情况下，该文件使用Axios HTTP库注册元标记的值。如果您不使用此库，则需要为应用程序手动配置此行为
+*/
+/*let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
+*/
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
